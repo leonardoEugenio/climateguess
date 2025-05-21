975684168c6d4bacdd7c3160855b0c88371103c7
leonardoEugenio <leonardo.emmantoni@gmail.com>
2025-05-21 16:26:39 -0300

    Feat: Create Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,35 @@
-import Image from "next/image";
+import Image from 'next/image'
+import southAmericaBgImg from '@/assets/images/south-america-bg.png'
+import { FaGlobe } from 'react-icons/fa'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <section className="w-screen h-screen relative">
+      <Image
+        src={southAmericaBgImg}
+        alt="mapa do sul"
+        className="object-cover size-full absolute top-0 left-0"
+      />
+      <main className="absolute size-full left-0 top-0 bg-radial-[at_50%_50%] from-slate-500/80 to-slate-800/10 dark:from-slate-800/80 backdrop-blur-sm">
+        <div className="flex flex-col items-center justify-center size-full">
+          <div className="bg-white dark:bg-slate-900 px-4 py-8 md:p-8 rounded-2xl shadow-2xl w-3xl max-w-[95vw] text-center space-y-8">
+            <div className="mx-auto w-fit flex items-center gap-2 text-2xl sm:text-5xl lg:text-7xl">
+              <FaGlobe className="text-secondary text-content" />
+              <h1 className="text-secondary dark:text-primary">
+                CLIMATE<span className="text-primary">GUESSER</span>{' '}
+              </h1>
+            </div>
+            <h2 className="text-slate-800 dark:text-slate-100">
+              Descubra a região mais próxima com base na previsão do tempo.
+              Analise as informações climáticas e veja se consegue acertar o
+              local!
+            </h2>
+            <button className="bg-secondary text-white p-4 font-bold mt-4 tracking-widest duration-300 hover:saturate-150 hover:scale-110">
+              JOGAR AGORA
+            </button>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    </section>
+  )
 }
